ff34509cbff299b25be02dab9cef8a3c14b4aa98
Ubuntu <shibahara@shibahara-ros2.z4rxojva5edehoxqqcqj5d1sld.lx.internal.cloudapp.net>
2025-12-01 09:44:38 +0000

    untracked

--- src/ros/build/turtlesim/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspace/src/ros/ros_tutorials/turtlesim
-# Build directory: /workspace/src/ros/build/turtlesim
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("turtlesim__py")
--- src/ros/build/turtlesim/turtlesim__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspace/src/ros/build/turtlesim/turtlesim__py
-# Build directory: /workspace/src/ros/build/turtlesim/turtlesim__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
